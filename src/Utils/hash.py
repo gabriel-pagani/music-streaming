86718392691b95b73ff14880e078efd5c9236ddd
Gabriel Souza <gabriel.souza@sinasc.com.br>
2025-03-13 09:04:27 -0300

    refactor: explicitei o tipo dos parâmetros e o tipo do retorno das funções

--- src/Utils/hash.py
+++ src/Utils/hash.py
@@ -1,12 +1,12 @@
 from bcrypt import gensalt, hashpw, checkpw
 
 
-def generate_hash(password):
+def generate_hash(password: str) -> bytes:
     # Gera um hash seguro para a senha
     salt = gensalt(round=12)
     return hashpw(password.encode(), salt)
 
 
-def verify_hash(password, hashed_password):
+def verify_hash(password: str, hashed_password: bytes) -> bool:
     # Verifica se a senha digitada confere com o hash armazenado
     return checkpw(password.encode(), hashed_password)
