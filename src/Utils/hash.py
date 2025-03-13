9afb9bb52d5fadd20e648e525bea54fb0a29091c
Gabriel Pagani <163437299+gabriel-pagani@users.noreply.github.com>
2025-03-13 09:24:29 -0300

    fix: corrigi o nome do parâmetro

--- src/Utils/hash.py
+++ src/Utils/hash.py
@@ -3,7 +3,7 @@ from bcrypt import gensalt, hashpw, checkpw
 
 def generate_hash(password: str) -> bytes:
     # Gera um hash seguro para a senha
-    salt = gensalt(round=12)
+    salt = gensalt(rounds=12)
     return hashpw(password.encode(), salt)
 
 
