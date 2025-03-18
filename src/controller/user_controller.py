b0e4472fa8dd79a5864b7c8367e3a2390a440bf9
Gabriel Souza <gabriel.souza@sinasc.com.br>
2025-03-18 11:19:59 -0300

    fix: removi o parâmetro query_type que não está mais sendo utilizado

--- src/controller/user_controller.py
+++ src/controller/user_controller.py
@@ -6,22 +6,17 @@ class UserController:
         pass
 
     def insert_user(self) -> dict:
-        self.response = server_request(
-            query_type='edit', query='insert into usuarios values ...')
+        self.response = server_request(query='insert into usuarios values ...')
         return self.response
 
     def select_users(self) -> dict:
-        self.response = server_request(
-            query_type='view', query='select * from usuarios'
-        )
+        self.response = server_request(query='select * from usuarios')
         return self.response
 
     def update_user(self) -> dict:
-        self.response = server_request(
-            query_type='edit', query='updete set usuarios ...')
+        self.response = server_request(query='updete set usuarios ...')
         return self.response
 
     def delete_user(self) -> dict:
-        self.response = server_request(
-            query_type='edit', query='delete from usuarios where ...')
+        self.response = server_request(query='delete from usuarios where ...')
         return self.response
