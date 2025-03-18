01b892b86f87c83df35b75dac21d7eba0309aeea
Gabriel Souza <gabriel.souza@sinasc.com.br>
2025-03-18 10:58:49 -0300

    perf: removi os parâmetros da função request_server e puxei as informações de login do arquivo .env

--- src/utils/conection.py
+++ src/utils/conection.py
@@ -1,20 +1,13 @@
 from pyodbc import connect, Error
+from os import getenv
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
-def server_request(server: str, database: str, query_type: str, query: str, user: str = '', password: str = '') -> dict:
-    # Args:
-    # server (str): Nome/endereço do servidor SQL
-    # database (str): Nome do banco de dados
-    # query_type (str): Tipo de consulta ('view' ou 'edit')
-    # query (str): Query SQL a ser executada
-    # user (str, opcional): Nome de usuário para autenticação
-    # password (str, opcional): Senha para autenticação
-
-    # Returns:
-    # dict: Dicionário contendo resultado da consulta e mensagem de status
-
+def server_request(query: str) -> dict:
     # Monta a string de conexão com o servidor
-    server_connection = f'DRIVER={{SQL Server}}; SERVER={server}; DATABASE={database}; UID={user}; PWD={password}'
+    server_connection = f'DRIVER={{SQL Server}}; SERVER={getenv("SERVER")}; DATABASE={getenv("DATABASE")}; UID={getenv("USER")}; PWD={getenv("PASSWORD")}'
     # Inicializa dicionário de resposta
     response = dict()
 
@@ -28,27 +21,21 @@ def server_request(server: str, database: str, query_type: str, query: str, user
                 # Executa a query
                 cursor.execute(formatted_query)
 
-                # Para consultas do tipo 'view' (SELECT)
-                if query_type.lower() == 'view':
+                # Para consultas do tipo (SELECT)
+                if query.lower().startswith('select'):
                     # Obtém todos os resultados
                     data = cursor.fetchall()
                     response['data'] = data
                     response['message'] = 'Consulta executada com sucesso!'
-                # Para consultas do tipo 'edit' (INSERT, UPDATE, DELETE)
-                elif query_type.lower() == 'edit':
+                # Para consultas do tipo (INSERT, UPDATE, DELETE)
+                else:
                     # Confirma as alterações no banco
                     connection.commit()
                     response['message'] = 'Script executado com sucesso!'
-                else:
-                    # Tipo de consulta inválido
-                    raise ValueError
 
     except Error as e:
         # Captura erros específicos de conexão com banco
         response['message'] = f'Erro de conexão: {e}'
-    except ValueError:
-        # Captura erro de tipo de consulta inválido
-        response['message'] = f'Tipo de consulta inválido!'
     except Exception as e:
         # Captura quaisquer outros erros não previstos
         response['message'] = f'Erro inesperado: {e}'
