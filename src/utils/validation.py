7c3065d2d4459db9f1499a8c6d9b406f9df904b7
gabriel-pagani <gabrielpaganidesouza@gmail.com>
2025-03-23 00:20:10 -0300

    feat: adicionei mais tipos de validação

--- src/utils/validation.py
+++ src/utils/validation.py
@@ -9,3 +9,18 @@ def validate_email(email):
 def validate_password(password):
     """Valida a força da senha."""
     return bool(search(r'^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%^&*(),.?":{}|<>]).{8,}$', password))
+
+
+def validate_zip_code(cep):
+    """Valida o formato do CEP."""
+    return bool(match(r'^\d{5}-?\d{3}$', cep))
+
+
+def validate_id(cpf):
+    """Valida o formato do CPF."""
+    return bool(match(r'^\d{3}\.?\d{3}\.?\d{3}-?\d{2}$', cpf))
+
+
+def validate_phone(phone):
+    """Valida o formato do número de telefone."""
+    return bool(search(r'^\(\d{2}\)\s?\d{4,5}-?\d{4}$', phone))
