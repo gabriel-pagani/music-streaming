bfcc0f7b879862138ff81d166fd53115c98a71a3
Gabriel Souza <gabriel.souza@sinasc.com.br>
2025-04-04 11:34:55 -0300

    fix: ajustei a classe User baseado na nova configuração do banco de dados

--- src/model/user.py
+++ src/model/user.py
@@ -5,18 +5,17 @@ from logging import error
 
 class User:
 
-    def __init__(self, email: str = None, password: str = None, name: str = None, id_number: str = None, birth_date: str = None, monthly_income: float = None,
-                 phone: str = None, state: str = None, city: str = None, neighborhood: str = None, street: str = None, number: int = None,
-                 complement: str = None, zip_code: str = None, id: int = None, score: int = None, status: str = None, user_type: str = None,
-                 registration_date: str = None, update_date: str = None, observations: str = None, card_number: str = None, card_name: str = None,
-                 card_valid_thru: str = None, card_code: int = None) -> None:
+    def __init__(self, id: int = None, email: str = None, password: str = None, name: str = None, id_number: str = None, phone: str = None, birth_date: str = None,
+                 state: str = None, city: str = None, neighborhood: str = None, street: str = None, number: int = None, complement: str = None, zip_code: str = None,
+                 card_number: str = None, card_name: str = None, card_valid_thru: str = None, card_code: int = None, last_digits_card: str = None,
+                 user_type: str = None, registration_date: str = None, update_date: str = None, observations: str = None) -> None:
+        self.id = id
         self.email = email
         self.password = password
         self.name = name
         self.id_number = id_number
-        self.birth_date = birth_date
-        self.monthly_income = monthly_income
         self.phone = phone
+        self.birth_date = birth_date
         self.state = state
         self.city = city
         self.neighborhood = neighborhood
@@ -28,10 +27,8 @@ class User:
         self.card_name = card_name
         self.card_valid_thru = card_valid_thru
         self.card_code = card_code
-        self.id = id
+        self.last_digits_card = last_digits_card
         self.user_type = user_type
-        self.score = score
-        self.status = status
         self.registration_date = registration_date
         self.update_date = update_date
         self.observations = observations
@@ -48,7 +45,7 @@ class User:
             else:
                 hashed_password = generate_hash(self.password)
                 server_request(
-                    query="INSERT INTO usuarios (email, hash_senha, nome) VALUES (?, ?, ?)",
+                    query="INSERT INTO usuarios (email, senha, nome) VALUES (?, ?, ?)",
                     params=(self.email, hashed_password, self.name)
                 )
                 return ['Success', 'Cadastro efetuado com sucesso!']
@@ -68,11 +65,12 @@ class User:
                 return ['Warning', 'Usuário inexistente!']
             else:
                 id = response['data'][0]['ID']
-                hash_senha = response['data'][0]['HASH_SENHA']
+                email = response['data'][0]['EMAIL']
+                hash_senha = response['data'][0]['SENHA']
                 name = response['data'][0]['NOME']
                 id_number = response['data'][0]['CPF']
-                birth_date = response['data'][0]['DATA_NASCIMENTO']
                 phone = response['data'][0]['TELEFONE']
+                birth_date = response['data'][0]['DATA_NASCIMENTO']
                 state = response['data'][0]['ESTADO']
                 city = response['data'][0]['CIDADE']
                 neighborhood = response['data'][0]['BAIRRO']
@@ -80,10 +78,12 @@ class User:
                 number = response['data'][0]['NUMERO']
                 complement = response['data'][0]['COMPLEMENTO']
                 zip_code = response['data'][0]['CEP']
-                card_number = response['data'][0]['NUMERO_CARTAO']
-                card_name = response['data'][0]['NOME_CARTAO']
-                card_valid_thru = response['data'][0]['VENCIMENTO_CARTAO']
-                card_code = response['data'][0]['CODIGO_CARTAO']
+                # Esses campos podem trazer algum erro de tipagem
+                # card_number = response['data'][0]['NUMERO_CARTAO']
+                # card_name = response['data'][0]['NOME_CARTAO']
+                # card_valid_thru = response['data'][0]['VENCIMENTO_CARTAO']
+                # card_code = response['data'][0]['CODIGO_CARTAO']
+                last_digits_card = response['data'][0]['ULTIMOS_DIGITOS_CARTAO']
                 user_type = response['data'][0]['TIPO']
                 registration_date = response['data'][0]['DATA_CADASTRO']
                 update_date = response['data'][0]['DATA_ATUALIZACAO']
@@ -91,10 +91,12 @@ class User:
 
                 if verify_hash(string=self.password, hash=hash_senha):
                     self.id = id
+                    self.email = email
+                    self.password = hash_senha
                     self.name = name
                     self.id_number = id_number
-                    self.birth_date = birth_date
                     self.phone = phone
+                    self.birth_date = birth_date
                     self.state = state
                     self.city = city
                     self.neighborhood = neighborhood
@@ -102,10 +104,12 @@ class User:
                     self.number = number
                     self.complement = complement
                     self.zip_code = zip_code
-                    self.card_number = card_number
-                    self.card_name = card_name
-                    self.card_valid_thru = card_valid_thru
-                    self.card_code = card_code
+                    # Esses campos podem trazer algum erro de tipagem
+                    # self.card_number = card_number
+                    # self.card_name = card_name
+                    # self.card_valid_thru = card_valid_thru
+                    # self.card_code = card_code
+                    self.last_digits_card = last_digits_card
                     self.user_type = user_type
                     self.registration_date = registration_date
                     self.update_date = update_date
@@ -124,10 +128,10 @@ class User:
             clause = ''
             values = list()
             fields = {
-                'hash_senha': self.password,
+                'senha': self.password,
                 'cpf': self.id_number,
-                'data_nascimento': self.birth_date,
                 'telefone': self.phone,
+                'data_nascimento': self.birth_date,
                 'estado': self.state,
                 'cidade': self.city,
                 'bairro': self.neighborhood,
@@ -135,10 +139,6 @@ class User:
                 'numero': self.number,
                 'complemento': self.complement,
                 'cep': self.zip_code,
-                'numero_cartao': self.card_number,
-                'nome_cartao': self.card_name,
-                'vencimento_cartao': self.card_valid_thru,
-                'codigo_cartao': self.card_code
             }
 
             for field, value in fields.items():
