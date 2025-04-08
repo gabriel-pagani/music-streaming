4fc41e586f87e82a8da7446e05f51011775fbd77
jpzyn <{jpstere25@gmail.com}(malito: jpstere25@gmail.com)>
2025-04-08 17:08:04 -0300

    test: Adicionei uma nova musica e imagem e configurei o audio_player com o flet

--- tests/audio_player.py
+++ tests/audio_player.py
@@ -10,17 +10,24 @@ class App:
 
     def setup_page(self) -> None:
         self.page.title = 'Sprobify'
-        self.page.window.height = 400
-        self.page.window.width = 400
+        self.page.window.height = 512
+        self.page.window.width = 512
         self.page.window.center()
         self.page.window.to_front()
         self.page.window.resizable = False
         self.page.window.maximizable = False
         self.page.theme_mode = ft.ThemeMode.LIGHT
         self.page.bgcolor = ft.Colors.WHITE
+        self.page.padding = 0
         self.audio = fta.Audio(
-            src="assets\\audios\\audio.mp3",
+            src="assets\\audios\\tu-tu-tu-du-max-verstappen.mp3",
             volume=10
+        )
+        self.background = ft.Image(
+            src="assets\\images\\max-logo.jpg",
+            fit=ft.ImageFit.COVER,
+            width=self.page.window.width,
+            height=self.page.window.height,
         )
         self.page.overlay.append(self.audio)
         self.page.update()
@@ -31,6 +38,15 @@ class App:
 
         def pause(e):
             self.audio.pause()
+
+        music_name = ft.Text(
+            value="Tu Tu Tu Du - Max Verstappen",
+            color=ft.Colors.WHITE,
+            size=3,
+            font_family="Arial",
+            weight=ft.FontWeight.BOLD,
+            text_align=ft.TextAlign.CENTER,
+        )
 
         play_button = ft.IconButton(
             icon=ft.Icons.PLAY_ARROW,
@@ -48,6 +64,7 @@ class App:
             on_click=pause,
         )
 
+
         # Layout
         content = ft.Row(
             controls=[
@@ -60,12 +77,30 @@ class App:
 
         container = ft.Container(
             content=content,
-            expand=True,
-            alignment=ft.alignment.center
+            expand=True,  # Garante que o container ocupe todo o espaço disponível
+            alignment=ft.alignment.bottom_center,  # Alinha o conteúdo na parte inferior e centralizado
+            padding=ft.Padding(0, 0, 0, 50)  # Adiciona um espaçamento de 50px acima da borda inferior
         )
 
-        self.page.add(container)
+        music_name_container = ft.Container(
+            content=music_name,
+            alignment=ft.alignment.top_center,  # Alinha o texto no topo e centralizado
+            padding=ft.Padding(10, 70, 10, 0),  # Adiciona espaçamento no topo
+            expand=True
+)
 
+        # Use Stack to overlay the background and controls
+        layout = ft.Stack(
+            controls=[
+                self.background,
+                music_name_container,  # Background image
+                container         # Botões centralizados
+            ],
+            expand=True  # Garante que o Stack ocupe todo o espaço da página
+        )
+
+        self.page.add(layout)
+        self.page.update()
 
 def main(page: ft.Page) -> None:
     App(page)
