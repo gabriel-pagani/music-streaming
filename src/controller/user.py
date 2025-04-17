84685a8253161d89c584764378cdce74e134d5aa
gabriel-pagani <gabrielpaganidesouza@gmail.com>
2025-04-16 22:06:31 -0300

    refactor: otimizei os métodos da classe User

--- src/controller/user.py
+++ src/controller/user.py
@@ -36,17 +36,17 @@ class User:
     def create_account(self) -> list:
         try:
             response = server_request(
-                query="SELECT COUNT(*) AS count FROM usuarios WHERE email = ?",
+                query="select * from usuarios where email = ?",
                 params=(self.email,)
             )
 
-            if response['data'][0]['count'] > 0:
+            if response['data'] != []:
                 return ['Warning', 'Este email já está cadastrado!']
             else:
-                hashed_password = generate_hash(self.password)
                 server_request(
-                    query="INSERT INTO usuarios (email, senha, nome) VALUES (?, ?, ?)",
-                    params=(self.email, hashed_password, self.name)
+                    query="insert into usuarios (email, nome, senha) values (?, ?, ?)",
+                    params=(self.email, self.name,
+                            generate_hash(self.password))
                 )
                 return ['Success', 'Cadastro efetuado com sucesso!']
 
@@ -57,63 +57,39 @@ class User:
     def login(self) -> list:
         try:
             response = server_request(
-                query="SELECT * FROM usuarios WHERE email = ?",
+                query="select * from usuarios where email = ?",
                 params=(self.email,)
             )
 
             if response['data'] == []:
                 return ['Warning', 'Usuário inexistente!']
             else:
-                id = response['data'][0]['ID']
-                email = response['data'][0]['EMAIL']
                 hash_senha = response['data'][0]['SENHA']
-                name = response['data'][0]['NOME']
-                id_number = response['data'][0]['CPF']
-                phone = response['data'][0]['TELEFONE']
-                birth_date = response['data'][0]['DATA_NASCIMENTO']
-                state = response['data'][0]['ESTADO']
-                city = response['data'][0]['CIDADE']
-                neighborhood = response['data'][0]['BAIRRO']
-                street = response['data'][0]['LOGRADOURO']
-                number = response['data'][0]['NUMERO']
-                complement = response['data'][0]['COMPLEMENTO']
-                zip_code = response['data'][0]['CEP']
-                # Esses campos podem trazer algum erro de tipagem
-                # card_number = response['data'][0]['NUMERO_CARTAO']
-                # card_name = response['data'][0]['NOME_CARTAO']
-                # card_valid_thru = response['data'][0]['VENCIMENTO_CARTAO']
-                # card_code = response['data'][0]['CODIGO_CARTAO']
-                last_digits_card = response['data'][0]['ULTIMOS_DIGITOS_CARTAO']
-                user_type = response['data'][0]['TIPO']
-                registration_date = response['data'][0]['DATA_CADASTRO']
-                update_date = response['data'][0]['DATA_ATUALIZACAO']
-                observations = response['data'][0]['OBSERVACOES']
-
                 if verify_hash(string=self.password, hash=hash_senha):
-                    self.id = id
-                    self.email = email
-                    self.password = hash_senha
-                    self.name = name
-                    self.id_number = id_number
-                    self.phone = phone
-                    self.birth_date = birth_date
-                    self.state = state
-                    self.city = city
-                    self.neighborhood = neighborhood
-                    self.street = street
-                    self.number = number
-                    self.complement = complement
-                    self.zip_code = zip_code
+                    self.id = response['data'][0]['ID']
+                    self.email = response['data'][0]['EMAIL']
+                    self.password = response['data'][0]['SENHA']
+                    self.name = response['data'][0]['NOME']
+                    self.id_number = response['data'][0]['CPF']
+                    self.phone = response['data'][0]['TELEFONE']
+                    self.birth_date = response['data'][0]['DATA_NASCIMENTO']
+                    self.state = response['data'][0]['ESTADO']
+                    self.city = response['data'][0]['CIDADE']
+                    self.neighborhood = response['data'][0]['BAIRRO']
+                    self.street = response['data'][0]['LOGRADOURO']
+                    self.number = response['data'][0]['NUMERO']
+                    self.complement = response['data'][0]['COMPLEMENTO']
+                    self.zip_code = response['data'][0]['CEP']
                     # Esses campos podem trazer algum erro de tipagem
-                    # self.card_number = card_number
-                    # self.card_name = card_name
-                    # self.card_valid_thru = card_valid_thru
-                    # self.card_code = card_code
-                    self.last_digits_card = last_digits_card
-                    self.user_type = user_type
-                    self.registration_date = registration_date
-                    self.update_date = update_date
-                    self.observations = observations
+                    # self.card_number = response['data'][0]['NUMERO_CARTAO']
+                    # self.card_name = response['data'][0]['NOME_CARTAO']
+                    # self.card_valid_thru = response['data'][0]['VENCIMENTO_CARTAO']
+                    # self.card_code = response['data'][0]['CODIGO_CARTAO']
+                    self.last_digits_card = response['data'][0]['ULTIMOS_DIGITOS_CARTAO']
+                    self.user_type = response['data'][0]['TIPO']
+                    self.registration_date = response['data'][0]['DATA_CADASTRO']
+                    self.update_date = response['data'][0]['DATA_ATUALIZACAO']
+                    self.observations = response['data'][0]['OBSERVACOES']
 
                     return ['Success', 'Login efetuado com sucesso!', self]
                 else:
@@ -153,7 +129,7 @@ class User:
 
             values.append(self.id)
 
-            query = f"UPDATE usuarios SET {clause.rstrip(', ')} WHERE id = ?"
+            query = f"update usuarios set {clause.rstrip(', ')} where id = ?"
 
             server_request(query=query, params=tuple(values))
 
